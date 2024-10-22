3f96df6c78058951fec3fd4dd0516da4515b094b
MrLexx1 <muhilman606@gmail.com>
2024-10-22 08:26:04 +0800

    update

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 // TUGAS KEDUA
+let pertama = prompt("isi angka awal \nContoh : 3");
+let kedua = prompt("isi angka awal \nContoh : 10");
+
 let output = "";
 
-for (let i = 1; i <= 9; i++) {
-  if (i % 2 === 1) {
-    output += i;
+for (let i = pertama; i <= kedua; i++) {
+  if (i % 2 === 0) {
+    output += "x ";
   } else {
-    output += " x ";
+    output += i + " ";
   }
 }
+alert(output);
 console.log(output);
